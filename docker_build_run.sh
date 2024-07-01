d2caaa142cac2bc9cb499c8c76cada31436534f4
Pedro Nunes <pedro.nunes@bjss.com>
2024-07-01 15:10:26 +0100

    create image only if not exists

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -1,2 +1,5 @@
-docker build . -t cdc
-docker run -p 8000:80 -v ./data:/cdc/data cdc
+if [ -z "$(docker images -q cdc:latest)" ]; then
+  echo Creating image
+  docker build . -t cdc
+fi
+docker run --rm -p 8000:80 -v ./data:/cdc/data cdc
