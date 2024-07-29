594e593bc1004ecf0004d00fc56e67f39ace36c1
Pedro Nunes <pedro.nunes@bjss.com>
2024-07-29 09:52:16 +0100

    add docker-compose.yml

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -1,14 +1,11 @@
 if [ -z "$(docker images -q cecc-base:latest)" ]; then
   echo Creating cecc-base image
-  #docker build -t cecc-base -f Dockerfile.base .
-  docker-compose build base
+  docker-compose build base || docker build -t cecc-base -f Dockerfile.base .
 fi
 
 if [ -z "$(docker images -q cecc:latest)" ]; then
   echo Creating cecc image
-  #docker build -t cecc .
-  docker-compose build app
+  docker-compose build app || docker build -t cecc .
 fi
 
-#docker run --rm -p 8443:443 -v ./data:/cecc/data cecc
-docker-compose run --rm --service-ports app
+docker-compose run --rm --service-ports app || docker run --rm -p 8443:443 -v ./data:/cecc/data cecc
