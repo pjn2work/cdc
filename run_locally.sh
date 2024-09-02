1492ddedbd1fa36a65a5e1392e5e69c2d97c8366
Pedro Nunes <pedro.nunes@bjss.com>
2024-09-02 18:08:45 +0100

    update name

--- run_locally.sh
+++ run_locally.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/cdc/apps/cdc || exit 1
+cd /home/cecc/apps/cdc || exit 1
 
 git pull
 
 source .venv/bin/activate
 
-uvicorn app.main:app --host 0.0.0.0 --port 8080 --log-config app/log.ini --reload --header App:CDC
+uvicorn app.main:app --host 0.0.0.0 --port 8080 --log-config app/log.ini --reload --header App:CECC
